d04c6879b4fb58c982c97897ef61467b41484777
WataruMorioka <wtr_morioka@icloud.com>
2019-08-05 12:42:50 +0900

    微修正

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,30 @@
 module.exports = {
     publicPath: '/tenth-project/',
     // publicPath: '',
-    outputDir: 'docs'
+    outputDir: 'docs',
+    pwa: {
+        workboxPluginMode: 'InjectManifest',
+        workboxOptions: {
+            swSrc: 'src/service-worker.js',
+            swDest: 'service-worker.js',
+            // globDirectory: path.join(__dirname, '/docs/'),
+            globPatterns: ['*.{js,css,json}'],
+            // runtimeCaching: [
+            //     {
+            //       urlPattern: /.*firebase.*/,
+            //       handler: 'networkFirst',
+            //       options: {
+            //         cacheName: 'api',
+            //       }
+            //     },
+            //     {
+            //       urlPattern: /\.(png|svg|woff|ttf|eot)/,
+            //       handler: 'cacheFirst',
+            //       options: {
+            //         cacheName: 'assets',
+            //       }
+            //     }        
+            //   ]    
+        }
+      }
 }
