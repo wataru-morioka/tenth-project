8285997ce7e8c3562568c9820c08f4035dde99ab
Wataru-morioka <wtr_morioka@icloud.com>
2019-08-05 11:58:22 +0900

    github-pages: update [skip ci]

--- precache-manifest.d72f93abf8c8cd5b6b077e4acee946cc.js
+++ precache-manifest.d72f93abf8c8cd5b6b077e4acee946cc.js
@@ -4,27 +4,27 @@ self.__precacheManifest = [
     "url": "/tenth-project/js/about.680774ca.js"
   },
   {
-    "revision": "972fb1bc4fdd935b2c07",
+    "revision": "1b6a25718dbfe1cd573b",
     "url": "/tenth-project/css/app.6cf83bc6.css"
   },
   {
-    "revision": "972fb1bc4fdd935b2c07",
-    "url": "/tenth-project/js/app.fdb28641.js"
+    "revision": "1b6a25718dbfe1cd573b",
+    "url": "/tenth-project/js/app.509ca1ef.js"
   },
   {
-    "revision": "84c8a7578c56e301f270",
-    "url": "/tenth-project/js/chunk-vendors.8bbeea9f.js"
+    "revision": "07ad45bb547a5ea626ac",
+    "url": "/tenth-project/js/chunk-vendors.fc25c2fe.js"
   },
   {
     "revision": "82b9c7a5a3f405032b1db71a25f67021",
     "url": "/tenth-project/img/logo.82b9c7a5.png"
   },
   {
-    "revision": "6c7377b5aef70d30b536a2c32d9217c3",
+    "revision": "2b846f00e8ec5016d04f48a7f65a57bf",
     "url": "/tenth-project/index.html"
   },
   {
-    "revision": "35ddc8c047e0860a361c0efc9e3472d6",
+    "revision": "3e1b8cf70b68d012d3cf0c3983e55fee",
     "url": "/tenth-project/firebase-messaging-sw.js"
   },
   {
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/tenth-project/precache-manifest.36c392ee785e89908231cbf5f92794a6.js"
+  "/tenth-project/precache-manifest.d72f93abf8c8cd5b6b077e4acee946cc.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "site"});
