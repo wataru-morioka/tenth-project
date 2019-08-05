5944d39be36f8cc9f6970129b80312e7e5e092b3
WataruMorioka <wtr_morioka@icloud.com>
2019-08-05 11:44:36 +0900

    微修正

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,8 +1,8 @@
-importScripts('https://www.gstatic.com/firebasejs/5.5.6/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/5.5.6/firebase-messaging.js');
 
 const ua = window.navigator.userAgent;
 if (ua.indexOf('Safari') !== -1) {
+    importScripts('https://www.gstatic.com/firebasejs/5.5.6/firebase-app.js');
+    importScripts('https://www.gstatic.com/firebasejs/5.5.6/firebase-messaging.js');
     // Initialize the Firebase app in the service worker by passing in the messagingSenderId.
     firebase.initializeApp({
         'messagingSenderId': '663686156877' // 4の messagingSenderId をコピペ
