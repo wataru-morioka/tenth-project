194b55432ccc3355ffb13556929b380e6d34988f
WataruMorioka <wtr_morioka@icloud.com>
2019-08-05 11:57:26 +0900

    微修正

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,15 +1,13 @@
+importScripts('https://www.gstatic.com/firebasejs/5.5.6/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/5.5.6/firebase-messaging.js');
 
-const ua = window.navigator.userAgent;
-if (ua.indexOf('Safari') !== -1) {
-    console.log(ua);
-    importScripts('https://www.gstatic.com/firebasejs/5.5.6/firebase-app.js');
-    importScripts('https://www.gstatic.com/firebasejs/5.5.6/firebase-messaging.js');
-    // Initialize the Firebase app in the service worker by passing in the messagingSenderId.
-    firebase.initializeApp({
-        'messagingSenderId': '663686156877' // 4の messagingSenderId をコピペ
-    });
-    const messaging = firebase.messaging();
-}
+console.log(ua);
+
+// Initialize the Firebase app in the service worker by passing in the messagingSenderId.
+firebase.initializeApp({
+    'messagingSenderId': '663686156877' // 4の messagingSenderId をコピペ
+});
+const messaging = firebase.messaging();
 
 // messaging.setBackgroundMessageHandler(function(payload) {
 //     const notificationTitle = payload.notification.title;
--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,9 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 import './registerServiceWorker';
-import firebase from 'firebase/app';
+import * as firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/messaging';
-
-const ua = window.navigator.userAgent;
 
 Vue.config.productionTip = false;
 
@@ -22,30 +20,27 @@ const firebaseConfig = {
 
 firebase.initializeApp(firebaseConfig);
 
-if (ua.indexOf('Safari') !== -1) {
-  console.log(ua);
-  // const test = require('https://www.gstatic.com/firebasejs/5.5.6/firebase-messaging.js');
-  const messaging = firebase.messaging();
-  messaging
-  .usePublicVapidKey('BH21Cxu2dV1d1gHd0nU-JzziDnfg-gtUIQEoSJKN6zsOcjThn7IVuVdsAtaVvF7ETjtW4SFvlvWZubj6-nHzrVg');
-  // 通知の受信許可
-  messaging.requestPermission().then(async () => {
-    console.log('Notification permission granted.');
-    // トークン取得
-    await messaging.getToken().then((token) => {
-      console.log('トークン取得：' + token);
-      if (token) {
-        // 本来ここでサーバにトークン送る処理
-        //sendTokenToServer(currentToken);
-    } else {
-        // トークン無い場合
-        alert('通知の許可をしていません。「通知を許可する」を押してください。');
-    }
-    })
-  }).catch((err) => {
-    console.log('Unable to get permission to notify.', err);
-  });
-}
+// const test = require('https://www.gstatic.com/firebasejs/5.5.6/firebase-messaging.js');
+const messaging = firebase.messaging();
+messaging
+.usePublicVapidKey('BH21Cxu2dV1d1gHd0nU-JzziDnfg-gtUIQEoSJKN6zsOcjThn7IVuVdsAtaVvF7ETjtW4SFvlvWZubj6-nHzrVg');
+// 通知の受信許可
+messaging.requestPermission().then(async () => {
+  console.log('Notification permission granted.');
+  // トークン取得
+  await messaging.getToken().then((token) => {
+    console.log('トークン取得：' + token);
+    if (token) {
+      // 本来ここでサーバにトークン送る処理
+      //sendTokenToServer(currentToken);
+  } else {
+      // トークン無い場合
+      alert('通知の許可をしていません。「通知を許可する」を押してください。');
+  }
+  })
+}).catch((err) => {
+  console.log('Unable to get permission to notify.', err);
+});
 
 new Vue({
   router,
