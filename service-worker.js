57a94e0ec629abe0054529f3e2230a7b67ef737c
WataruMorioka <wtr_morioka@icloud.com>
2019-08-07 00:48:21 +0900

    Updates

--- service-worker.js
+++ service-worker.js
@@ -1,33 +1,22 @@
-/**
- * Welcome to your Workbox-powered service worker!
- *
- * You'll need to register this file in your web app and you should
- * disable HTTP caching for this file too.
- * See https://goo.gl/nhQhGp
- *
- * The rest of the code is auto-generated. Please don't update this file
- * directly; instead, make changes to your Workbox build configuration
- * and re-run your build process.
- * See https://goo.gl/2aRDsh
- */
-
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
-
-importScripts(
-  "/tenth-project/precache-manifest.e78b077dc60dcac217a153e327614ff6.js"
-);
+importScripts("/tenth-project/precache-manifest.e78b077dc60dcac217a153e327614ff6.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 workbox.core.setCacheNameDetails({prefix: "v2"});
 
+// First, import the library into the service worker global scope:
+importScripts('path/to/offline-google-analytics-import.js');
+
+// Then, call goog.offlineGoogleAnalytics.initialize():
+// See https://github.com/GoogleChrome/workbox/tree/master/packages/workbox-google-analytics
+goog.offlineGoogleAnalytics.initialize();
+
 workbox.skipWaiting();
 workbox.clientsClaim();
-
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
  * requests for URLs in the manifest.
  * See https://goo.gl/S9QRab
  */
-self.__precacheManifest = [].concat(self.__precacheManifest || []);
+self.__precacheManifest = [].concat(self.__precacheManifest || ['/img/**/*.png']);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
