3697cc8183db0c0b85c2c3120a61bd6beb8f002a
WataruMorioka <wtr_morioka@icloud.com>
2019-08-21 17:58:22 +0900

    Updates

---
+++ precache-manifest.b03c2a85521410d5941db43e58ab4616.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "ca3afb4defbaa8bedbf5",
+    "url": "/tenth-project/js/about.c0a90741.js"
+  },
+  {
+    "revision": "ff715b33e838a56832cc",
+    "url": "/tenth-project/css/app.2e9262a8.css"
+  },
+  {
+    "revision": "ff715b33e838a56832cc",
+    "url": "/tenth-project/js/app.a7ddc5e6.js"
+  },
+  {
+    "revision": "d2cf2872ae0beb0658e9",
+    "url": "/tenth-project/js/chunk-vendors.c45b36d8.js"
+  },
+  {
+    "revision": "d2f71912a28e7ca4b44b19ff01965edb",
+    "url": "/tenth-project/media/jagermeister.d2f71912.mp4"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/tenth-project/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "d984391eb79abec43dd347e1e467b685",
+    "url": "/tenth-project/index.html"
+  },
+  {
+    "revision": "19ac9a5d46e7b607e2049845e41a3cd4",
+    "url": "/tenth-project/firebase-messaging-sw.js"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/tenth-project/robots.txt"
+  }
+];
